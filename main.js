35469dab6f4c2e6ed23f201760be94e9a585849f
Erika Robles Araya <12820548+ekra26@users.noreply.github.com>
2021-02-26 18:13:55 -0500

    MVP with all working functions

--- main.js
+++ main.js
@@ -22,13 +22,85 @@ const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3]
 // An array of all the arrays above
 const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5]
 
+//New credit cards to check
+const batch2 = [];
 
-// Add your functions below:
+// functions:
+
+//Check if credit card number is valid or not, return true for valid credit card
+function validateCred(creditCardNumber) {
+    const creditCardLengthEven = creditCardNumber.length % 2 === 0;
+    let doubledCreditNumber = [];
+    for (let i = creditCardNumber.length - 1; i >= 0; i--) {
+           if (creditCardLengthEven) {
+               if (i % 2 == 0) {
+                let doubledSingleNumber = creditCardNumber[i]*2;
+                let doubledSingleNumberToAdd = doubledSingleNumber > 9 ? doubledSingleNumber - 9 : doubledSingleNumber;
+                doubledCreditNumber.unshift(doubledSingleNumberToAdd);
+               } else {
+               doubledCreditNumber.unshift(creditCardNumber[i]);
+            }
+        } else if (i % 2 !== 0) {
+               let doubledSingleNumber = creditCardNumber[i]*2;
+               let doubledSingleNumberToAdd = doubledSingleNumber > 9 ? doubledSingleNumber - 9 : doubledSingleNumber;
+               doubledCreditNumber.unshift(doubledSingleNumberToAdd);
+           } else {
+            doubledCreditNumber.unshift(creditCardNumber[i]);
+        }
+    }     
+    let sumCreditNumber = doubledCreditNumber.reduce((acc, val) => acc + val, 0);
+    let validCreditCard = sumCreditNumber % 10 === 0;
+    return validCreditCard;
+}
 
 
+//find all invalid cards from one nested array, return a new array of invalid cards
+function findInvalidCards(creditCardBatch) {
+    const invalidCreditCardBatch = [];
+    creditCardBatch.forEach(creditCardNumber => {
+        if (!validateCred(creditCardNumber)) {
+            invalidCreditCardBatch.push(creditCardNumber);
+        }
+    })
+   return invalidCreditCardBatch;
+}
 
+//Identify credit card company with invalid cards
+function idInvalidCardCompanies() {
+    let invalidCreditCards = findInvalidCards(batch);
+    console.log(invalidCreditCards);
+    let numberOfInvalidCreditCards = invalidCreditCards.length;
+    let fistCreditCardNumberValue = [];
+    for (let i = numberOfInvalidCreditCards -1; i >= 0; i--) {
+        fistCreditCardNumberValue.push(invalidCreditCards[i][0]); 
+    }
+    let uniquitefirstCreditCardNumberValue = new Set (fistCreditCardNumberValue);
+    let creditCardCompanieswithInvalidCards = [];
+    uniquitefirstCreditCardNumberValue.forEach(numberValue => {
+        switch (true) {
+            case numberValue === 3:
+                creditCardCompanieswithInvalidCards.push(' Amex');
+                break;
+            case numberValue === 4:
+                creditCardCompanieswithInvalidCards.push(' Visa');
+                break;
+            case numberValue === 5:
+                creditCardCompanieswithInvalidCards.push(' MasterCard');
+                break;
+            case numberValue === 6:
+                creditCardCompanieswithInvalidCards.push(' Discover');
+                break;
+            default:
+                creditCardCompanieswithInvalidCards.push('Company not found');
+        }
+    })
+    return console.log(`Credit card companies with invalid credit card numbers: ${creditCardCompanieswithInvalidCards}`)
+};
 
-
-
-
-
+//function accept string and convert to array
+function convertStringtoArray(creditCardstring){
+    let newCreditCardInput = creditCardstring;
+    let newCreditCardArray = newCreditCardInput.split("").map((newCreditCardInput)=>{
+        return Number(newCreditCardInput)});
+    return batch2.push(newCreditCardArray);
+}
